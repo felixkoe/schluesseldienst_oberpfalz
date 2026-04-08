e82d74afa9da435d68fa47c35cd8cf8f22b9a643
Felix Koelling <felix.koelling@hotmail.de>
2026-04-08 19:10:00 +0200

    v1

--- src/assets/siteImages.ts
+++ src/assets/siteImages.ts
@@ -1,13 +1,13 @@
-import heroImage from "./WhatsApp Image 2026-02-28 at 06.57.05 (2).jpeg";
-import aboutImage from "./WhatsApp Image 2026-02-28 at 06.57.08 (1).jpeg";
-import serviceDoorOpeningImage from "./WhatsApp Image 2026-02-28 at 06.57.05 (1).jpeg";
-import serviceCylinderImage from "./WhatsApp Image 2026-02-28 at 06.57.05 (4).jpeg";
-import serviceSecurityImage from "./WhatsApp Image 2026-02-28 at 06.57.09 (10).jpeg";
-import serviceSpecialOpeningsImage from "./WhatsApp Image 2026-02-28 at 06.57.05 (3).jpeg";
-import serviceAreaImageOne from "./WhatsApp Image 2026-02-28 at 06.57.06 (2).jpeg";
-import serviceAreaImageTwo from "./WhatsApp Image 2026-02-28 at 06.57.06 (4).jpeg";
-import serviceAreaImageThree from "./WhatsApp Image 2026-02-28 at 06.57.08 (1).jpeg";
-import serviceAreaImageFour from "./WhatsApp Image 2026-02-28 at 06.57.06 (5).jpeg";
+import heroImage from "./images/FloAuto.jpeg";
+import aboutImage from "./images/WhatsApp Image 2026-02-28 at 06.57.08 (1).jpeg";
+import serviceDoorOpeningImage from "./images/WhatsApp Image 2026-02-28 at 06.57.05 (1).jpeg";
+import serviceCylinderImage from "./images/WhatsApp Image 2026-02-28 at 06.57.05 (4).jpeg";
+import serviceSecurityImage from "./images/WhatsApp Image 2026-02-28 at 06.57.09 (10).jpeg";
+import serviceSpecialOpeningsImage from "./images/WhatsApp Image 2026-02-28 at 06.57.05 (3).jpeg";
+import serviceAreaImageOne from "./images/WhatsApp Image 2026-02-28 at 06.57.06 (2).jpeg";
+import serviceAreaImageTwo from "./images/WhatsApp Image 2026-02-28 at 06.57.06 (4).jpeg";
+import serviceAreaImageThree from "./images/WhatsApp Image 2026-02-28 at 06.57.08 (1).jpeg";
+import serviceAreaImageFour from "./images/WhatsApp Image 2026-02-28 at 06.57.06 (5).jpeg";
 
 export {
   aboutImage,
